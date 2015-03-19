fe2f9fcf904a4e97d143b2689049a1e8823d1cab
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-03-19 12:17:05 -0400

    Animates Hex Overlay

--- site/js/src/atomic-generator.js
+++ site/js/src/atomic-generator.js
@@ -1,27 +1,27 @@
 // Config
 // ============================================================
 
-var $tks_html         = $('pre[class="language-html"] > code'),
-    $tks_js           = $('.demo-js'),
-    tks_htmlsnippets  = [],
-    tks_js_snippets   = [];
+var $atomic_html       = $('pre[class="language-html"] > code'),
+    $atomic_js         = $('.atomic-js'),
+    atomic_htmlsnippet = [],
+    atomic_jsnippet    = [];
 
 
 // Markup
 // order matters!
 // ============================================================
 
-$tks_html.each(function(i) {
+$atomic_html.each(function(i) {
 
-  tks_htmlsnippets.push(
-    $tks_html[i]
+  atomic_htmlsnippet.push(
+    $atomic_html[i]
       .innerHTML
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;')
   );
 
-  $(this).html(tks_htmlsnippets[i]);
+  $(this).html(atomic_htmlsnippet[i]);
 
 });
 
@@ -30,10 +30,10 @@ $tks_html.each(function(i) {
 // order matters!
 // ============================================================
 
-$tks_js.each(function(i) {
+$atomic_js.each(function(i) {
 
-  tks_js_snippets.push(
-    $tks_js[i]
+  atomic_jsnippet.push(
+    $atomic_js[i]
       .innerHTML
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
@@ -43,5 +43,5 @@ $tks_js.each(function(i) {
 });
 
 $('pre[class="language-javascript"] > code').each(function(i) {
-  $(this).append(tks_js_snippets[i])
+  $(this).append(atomic_jsnippet[i])
 });
--- site/js/src/atomic-swatch.js
+++ site/js/src/atomic-swatch.js
@@ -1,3 +1,15 @@
+// DOM Insertion
+// ======================================================
+
+$('body.atomic').append('<div class="atomic-hexoverlay"></div>');
+
+
+// References
+// ======================================================
+
+var $atomic_hexoverlay = $('.atomic-hexoverlay');
+
+
 // RGB2Hex Converter
 // ======================================================
 
@@ -9,6 +21,11 @@ function atomicRgb2Hex(rgb){
   ('0' + parseInt(rgb[2], 10).toString(16)).slice(-2) +
   ('0' + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
 }
+
+// http://www.w3.org/TR/cssom/#serialize-a-css-value
+// $('#colors svg').each(function(i) {
+//   console.log(window.getComputedStyle(this).getPropertyValue('fill'));
+// });
 
 
 // Color Swatch Event
@@ -24,9 +41,11 @@ $('#atomic-colors svg').on('click', function(e) {
                                 .replace(/ /g, '')
                                 .split(',');
 
-  $('body.atomic').append('<div class="atomic-swatch"><p>' + atomic_hexvalue + '</p><b>Click Hex Code to Copy</b></div>');
+  $atomic_hexoverlay.addClass('_active');
 
-  $('.atomic-swatch').css({
+  $atomic_hexoverlay.html('<p>' + atomic_hexvalue + '</p><b>Click Hex Code to Copy</b>');
+
+  $atomic_hexoverlay.css({
     'background': 'rgba('+ atomic_rgbchannel[0] + ',' + atomic_rgbchannel[1] + ',' + atomic_rgbchannel[2] + ', 0.95)'
   });
 
@@ -36,13 +55,20 @@ $('#atomic-colors svg').on('click', function(e) {
 // Clipboard Event
 // ======================================================
 
-$('body.atomic').on('click', '.atomic-swatch', function(e) {
+$atomic_hexoverlay.on('click', function(e) {
 
-  if(e.target.nodeName !== 'P') { $(this).remove(); }
+  var value = atomicRgb2Hex( $atomic_hexoverlay.css('background') );
+
+  if(e.target.nodeName !== 'P') {
+    $atomic_hexoverlay.removeClass('_active');
+  }
 
   if(e.target.nodeName === 'P') {
-    var atomic_hexvalue = window.prompt("Copy to clipboard: CTRL+C (CMD+C on macs)", atomicRgb2Hex( $('.atomic-swatch').css('background') ));
-    if(atomic_hexvalue !== null) { $(this).remove(); }
+    var atomic_hexprompt = window.prompt("Copy to clipboard: CTRL+C (CMD+C on macs)", atomicRgb2Hex( $('.atomic-swatch').css('background') ));
+
+    if(atomic_hexprompt !== null) {
+      $atomic_hexoverlay.removeClass('_active');
+    }
   }
 
 });
